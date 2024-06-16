a402d7e9fb4b43ce390b61caedb98ef61b1df008
taegyeong0225 <202244042@itc.ac.kr>
2024-06-16 23:52:08 +0900

    리뷰 등록에 글 작성 버튼 추가

--- app/src/main/java/com/example/android_final_app/RegisterFragment.java
+++ app/src/main/java/com/example/android_final_app/RegisterFragment.java
@@ -39,7 +39,7 @@ public class RegisterFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
 
         // 리사이클러 뷰 초기화
         recyclerView = view.findViewById(R.id.recyclerViewPosts);
