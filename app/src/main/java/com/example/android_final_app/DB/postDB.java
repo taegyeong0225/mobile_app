2a9c7ac42d91e439c5b282dd0812d0f387783ed6
taegyeong0225 <202244042@itc.ac.kr>
2024-06-16 12:53:31 +0900

    post 관련 DB 생성

---
+++ app/src/main/java/com/example/android_final_app/DB/postDB.java
@@ -0,0 +1,11 @@
+package com.example.android_final_app.DB;
+
+public class postDB {
+    public static final String TABLE_POST = "post";
+
+    public static final String COL_NO = "postID";
+    public static final String COL_TITLE = "title";
+    public static final String COL_CONTENT = "content";
+    public static final String COL_AUTHOR = "author";
+    public static final String COL_RESTAURANT = "restaurant";
+}
---
+++ app/src/main/java/com/example/android_final_app/DB/postDBHelper.java
@@ -0,0 +1,58 @@
+package com.example.android_final_app.DB;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class postDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "MOBILE_FINAL_PROJECT.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public postDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_POSTS_TABLE = "CREATE TABLE " + postDB.TABLE_POST + " (" +
+                postDB.COL_NO + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                postDB.COL_TITLE + " TEXT, " +
+                postDB.COL_CONTENT + " TEXT, " +
+                postDB.COL_AUTHOR + " TEXT, " +
+                postDB.COL_RESTAURANT + " TEXT)";
+        db.execSQL(CREATE_POSTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + postDB.TABLE_POST);
+        onCreate(db);
+    }
+
+    // 데이터 삽입 메서드
+    public long addPost(String title, String content, String author, String restaurant) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(postDB.COL_TITLE, title);
+        values.put(postDB.COL_CONTENT, content);
+        values.put(postDB.COL_AUTHOR, author);
+        values.put(postDB.COL_RESTAURANT, restaurant);
+        return db.insert(postDB.TABLE_POST, null, values);
+    }
+
+    // 데이터 읽기 메서드
+    public Cursor getAllPosts() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {
+                postDB.COL_NO,
+                postDB.COL_TITLE,
+                postDB.COL_CONTENT,
+                postDB.COL_AUTHOR,
+                postDB.COL_RESTAURANT
+        };
+        return db.query(postDB.TABLE_POST, projection, null, null, null, null, null);
+    }
+}
